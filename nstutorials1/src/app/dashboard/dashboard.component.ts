be5b9176ddb9c49dfea268be293e0a97dc24276b
Subhra Roy <subhra.roy84@yahoo.in>
2021-03-17 16:00:44 +0530

    Add navigation with multiple data transfer

--- nstutorials1/src/app/dashboard/dashboard.component.ts
+++ nstutorials1/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LoginService } from '../app-service/login.service';
 import { Location } from '@angular/common';
+import { RouterExtensions } from '@nativescript/angular';
 
 
 @Component({
@@ -15,21 +16,30 @@ export class DashboardComponent implements OnInit {
   public firstName: String
   public lastName: String
 
-  constructor(private activeRouter: ActivatedRoute,
+  constructor(private activeRouter: RouterExtensions,
               private location: Location,
-              private logService: LoginService) { }
+              private logService: LoginService) {
+
+    let state = this.activeRouter.router.getCurrentNavigation().extras.state
+   //console.log(state["user_email"])
+    //console.log(state["user_token"])
+    this.userEmail = state["user_email"]
+    this.firstName = state["user_firstName"]
+    this.lastName = state["user_lastName"]
+
+  }
 
   ngOnInit() {
-    this.onLoad()
+    //this.onLoad()
   }
 
-  private onLoad(){
-    this.activeRouter.params.subscribe((params) => {
-      let _token = params["token"] as string
-      console.log(_token)
-    })
+//   private onLoad(){
+//     this.activeRouter.params.subscribe((params) => {
+//       let _token = params["token"] as string
+//       console.log(_token)
+//     })
 
-  }
+//   }
 
   goBack(){
     this.logService.resetToken()
--- nstutorials1/src/app/login/login.component.ts
+++ nstutorials1/src/app/login/login.component.ts
@@ -3,24 +3,25 @@ import { LoginService } from '../app-service/login.service';
 import { EventData } from '@nativescript/core/data/observable';
 //import { Button } from '@nativescript/core/ui';
 import { LoginModel } from '../app-interface/loginModel';
-import { Router } from '@angular/router';
-
+//import { Router } from '@angular/router';
+import { RouterExtensions } from '@nativescript/angular';
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
+
 export class LoginComponent implements OnInit {
   public loginModel: LoginModel
-  
-  constructor(private loginService: LoginService, private route: Router) {
+  constructor(private loginService: LoginService,
+            private route: RouterExtensions) {
     this.loginModel = { email: "", password: "", token: ""}
-    
+
   }
 
   ngOnInit() {
-    
+
   }
 
   onClickLogin(args: EventData){
@@ -29,8 +30,14 @@ export class LoginComponent implements OnInit {
     console.log(this.loginModel)
     this.loginService.setLoginModel(this.loginModel.email, this.loginModel.password)
     this.loginModel = this.loginService.getLoginModel()
-    this.route.navigate(["dashboard", this.loginModel.token])
-
+    this.route.navigate(["dashboard", this.loginModel.token], {
+        state: {
+            user_email: this.loginModel.email,
+            user_token: this.loginModel.token,
+            user_firstName: "Subhra",
+            user_lastName: "Roy"
+        }
+    })
   }
 
 }
