50c88b3f097e02686562eb3ee1d024f36887e6aa
Subhra Roy <subhra.roy84@yahoo.in>
2021-03-23 18:05:13 +0530

    add new project

---
+++ groceries/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from './app-modules/login/login.component';
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { NativeScriptRouterModule } from "@nativescript/angular";
+
+const routes: Routes = [
+    { path: "", redirectTo: "/login", pathMatch: "full" },
+    { path: "login", component: LoginComponent },
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
---
+++ groceries/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NativeScriptFormsModule } from "@nativescript/angular";
+import { LoginComponent } from './app-modules/login/login.component';
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "@nativescript/angular";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule,
+        NativeScriptFormsModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    providers: [],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
