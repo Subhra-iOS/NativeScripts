2683fa8894d881711bb74536946adc0e1ffbc1e4
Subhra Roy <subhra.roy84@yahoo.in>
2021-03-27 22:00:31 +0530

    add list service

--- groceries/src/app/app-modules/list/list.component.ts
+++ groceries/src/app/app-modules/list/list.component.ts
@@ -1,22 +1,42 @@
+import { Grocery } from './../../shared/grocery/grocery';
 import { Component, OnInit } from '@angular/core';
+import { GroceryListService } from '~/app/shared/grocery/grocery-list.service';
 
 @Component({
   selector: 'ns-list',
+  providers: [GroceryListService],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css', './list-common.css']
 })
 export class ListComponent implements OnInit {
 
-  public groceryList = []
+  public groceryList: Grocery[] = []
 
-  constructor() {
+  constructor(private groceryService: GroceryListService) {
 
   }
 
   public ngOnInit() {
-      this.groceryList.push({ name: 'Apple'})
-      this.groceryList.push({ name: 'Banana'})
-      this.groceryList.push({ name: 'Lemon'})
+    this.groceryService.loadData().subscribe(
+         result => {
+            console.log(result)
+            result.forEach( grocery => {
+              this.groceryList.unshift(grocery)
+            })
+         },
+         error => alert("Error in service")
+    )
+    // this.groceryService.loadList()
+    //     .subscribe(
+    //         result => {
+    //         console.log(result)
+    //         result.forEach( grocery => {
+    //             this.groceryList.unshift(grocery)
+    //         })
+    //         },
+    //         error => alert("Error in service")
+    //     )
+
   }
 
 }
--- groceries/src/app/shared/grocery/grocery-list.service.ts
+++ groceries/src/app/shared/grocery/grocery-list.service.ts
@@ -1,10 +1,65 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Config } from '../config';
+import { map, catchError } from "rxjs/operators";
+import { throwError, Observable } from 'rxjs';
+import { Grocery } from './grocery';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroceryListService {
 
-constructor() { }
+    private baseUrl = Config.apiUrl + "appdata/" + Config.appKey + "/Groceries"
+
+    constructor(private http: HttpClient) {
+
+    }
+    public loadData(){
+        return this.http.get(
+            this.baseUrl,
+            { headers: this.getCommonHeaders() }
+        ).pipe(
+            map( (data: any[]) => {
+                const groceryList = data
+                                    .sort((a, b) => {
+                                        return a._kmd.lmt > b._kmd.lmt ? -1 : 1
+                                    })
+                                    .map( groceryModel => new Grocery(
+                                        groceryModel.id,
+                                        groceryModel.name
+                                    ))
+
+                return groceryList
+            },
+            (error: any) => {
+                console.log(error)
+            }),
+            catchError(this.handleErrors)
+        )
+    }
+
+    public loadList(): Observable<Grocery[]> {
+        return this.http.get<Grocery[]>(
+            this.baseUrl,
+            { headers: this.getCommonHeaders() }
+        )
+    }
+
+
+
+    private handleErrors(error: HttpErrorResponse){
+        console.log(error)
+        return throwError(error)
+    }
+
+    private getCommonHeaders() {
+        return new HttpHeaders({
+            "Content-Type": "application/json",
+            "Authorization": "Kinvey " + Config.token,
+        });
+    }
 
 }
+
